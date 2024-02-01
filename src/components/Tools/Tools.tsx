5d90bb8958fa9180f56084c9cfe89fe1396a5161
MarineLeBec <marinelebec@gmail.com>
2024-02-01 18:15:15 +0100

    feat: fading in tools logos at hover

--- src/components/Tools/Tools.tsx
+++ src/components/Tools/Tools.tsx
@@ -1,13 +1,27 @@
-import '../App/reset.css'
-import './Tools.scss'
+import "../App/reset.css";
+import "./Tools.scss";
+import { useState } from "react";
+import html_logo from "./../../assets/icons8-html-5-144.png";
+import css_logo from "./../../assets/icons8-css3-144.png";
+import js_logo from "./../../assets/icons8-javascript-144.png"
 
 function Tools() {
+      const [isHovered, setIsHovered] = useState(false);
 
-    return (
-        <section className='tools'>
-            <h3>Tools</h3>
-        </section>
-    )
+  return (
+    <section
+      className={`tools ${isHovered ? "hovered" : ""}`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <h3>Tools</h3>
+      <div className={`tools-logos ${isHovered ? "visible" : ""}`}>
+        <img src={html_logo} alt="Logo HTML" />
+        <img src={css_logo} alt="Logo CSS" />
+        <img src={js_logo} alt="Logo JavaScript" />
+      </div>
+    </section>
+  );
 }
 
 export default Tools;
