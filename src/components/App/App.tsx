b00938ec48afe41aabbac75f19e804af800ba423
MarineLeBec <marinelebec@gmail.com>
2024-02-03 14:08:20 +0100

    feat: adding tools logos in assets and laying them out in section

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,22 +1,22 @@
 //* Styles
-import './reset.css'
-import './App.scss'
+import "./reset.css";
+import "./App.scss";
 
 //* Hooks
-import { useState } from 'react';
+import { useState } from "react";
 
 //* Composants
-import Tools from '../Tools/Tools';
+import Tools from "../Tools/Tools";
+//* -------------------------------------------------------- */
 
 function App() {
-  //TODO Animate Title at hover
   const [isHovered, setIsHovered] = useState(false);
 
   return (
     <>
       <header>
         <section
-          className={`title ${isHovered ? 'hovered' : ''}`}
+          className={`title ${isHovered ? "hovered" : ""}`}
           onMouseEnter={() => setIsHovered(true)}
           onMouseLeave={() => setIsHovered(false)}
         >
@@ -24,32 +24,32 @@ function App() {
           <h2>Développeuse web</h2>
           <h2>full-stack</h2>
         </section>
-          <Tools />
+        <Tools />
       </header>
       <main>
-        <section className='projects'>
+        <section className="projects">
           <h3>Projects</h3>
         </section>
-        <section className='blog'>
+        <section className="blog">
           <h3>Blog</h3>
         </section>
-        <section className='resume'>
+        <section className="resume">
           <h3>CV</h3>
         </section>
-        <section className='about'>
+        <section className="about">
           <h3>About</h3>
         </section>
       </main>
       <footer>
-        <section className='contact'>
+        <section className="contact">
           <h3>Contact</h3>
         </section>
-        <section className='social'>
+        <section className="social">
           <h3>Social</h3>
         </section>
       </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Tools/Tools.tsx
+++ src/components/Tools/Tools.tsx
@@ -1,12 +1,34 @@
+//* Styles
 import "../App/reset.css";
 import "./Tools.scss";
+//* Hooks
 import { useState } from "react";
+//* Logos
 import html_logo from "./../../assets/icons8-html-5-144.png";
 import css_logo from "./../../assets/icons8-css3-144.png";
-import js_logo from "./../../assets/icons8-javascript-144.png"
+import js_logo from "./../../assets/icons8-javascript-144.png";
+import ts_logo from "./../../assets/icons8-typescript-144.png";
+import tailwind_logo from "./../../assets/icons8-tailwind-css-144.png";
+import wp_logo from "./../../assets/icons8-wordpress-144.png";
+import react_logo from "./../../assets/icons8-react-200.png";
+import php_logo from "./../../assets/icons8-php-logo-144.png";
+import git_logo from "./../../assets/icons8-git-144.png";
+
+const logos = [
+  { id: 1, src: html_logo, alt: "Logo HTML" },
+  { id: 2, src: css_logo, alt: "Logo CSS" },
+  { id: 3, src: js_logo, alt: "Logo JavaScript" },
+  { id: 4, src: ts_logo, alt: "Logo TypeScript" },
+  { id: 5, src: tailwind_logo, alt: "Logo Tailwind" },
+  { id: 6, src: wp_logo, alt: "Logo Wordpress" },
+  { id: 7, src: react_logo, alt: "Logo React" },
+  { id: 8, src: php_logo, alt: "Logo PHP" },
+  { id: 9, src: git_logo, alt: "Logo Git" },
+];
+//* -------------------------------------------------------- */
 
 function Tools() {
-      const [isHovered, setIsHovered] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
 
   return (
     <section
@@ -16,9 +38,9 @@ function Tools() {
     >
       <h3>Tools</h3>
       <div className={`tools-logos ${isHovered ? "visible" : ""}`}>
-        <img src={html_logo} alt="Logo HTML" />
-        <img src={css_logo} alt="Logo CSS" />
-        <img src={js_logo} alt="Logo JavaScript" />
+        {logos.map((logo) => (
+          <img key={logo.id} src={logo.src} alt={logo.alt} />
+        ))}
       </div>
     </section>
   );
