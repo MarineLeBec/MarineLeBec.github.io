9aa379d5aa557b9ed6e0fca6ca32c1df10245795
MarineLeBec <marinelebec@gmail.com>
2024-02-09 18:17:36 +0100

    feat: filling projects section

--- src/components/Main/Projects.tsx
+++ src/components/Main/Projects.tsx
@@ -2,20 +2,51 @@
 import "../App/reset.css";
 import "./Projects.scss";
 //* Hooks
-// import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Projects: React.FC = () => {
+  const location = useLocation();
+  const isLink = location.pathname === "/";
+
+  if (isLink) {
+    return (
+      <section className="projects">
+        <h3>Projects</h3>
+        <button>
+          <Link to="/projects">
+            <img
+              width="50"
+              height="50"
+              src="https://img.icons8.com/sf-black-filled/64/FFFFFF/plus.png"
+              alt="plus"
+            />
+          </Link>
+        </button>
+
+      </section>
+    );
+  }
+
   return (
     <section className="projects">
       <h3>Projects</h3>
       <button>
-        <img
-          width="50"
-          height="50"
-          src="https://img.icons8.com/sf-black-filled/64/FFFFFF/plus.png"
-          alt="plus"
-        />
+        <Link to="/">
+          <img
+            width="64"
+            height="64"
+            src="https://img.icons8.com/sf-black-filled/64/FFFFFF/cancel.png"
+            alt="cancel"
+          />
+        </Link>
       </button>
+      <div className="projects-list">
+        <ul>
+          <li>Movie scene generator</li>
+          <li>My Portfolio</li>
+          <li>Apothéose</li>
+        </ul>
+      </div>
     </section>
   );
 };
