2f6ee522896b6056432fde22acf032bf3e1ca336
MarineLeBec <marinelebec@gmail.com>
2024-02-19 11:35:01 +0100

    feat: adding projects to projects section

--- src/components/Main/Projects.tsx
+++ src/components/Main/Projects.tsx
@@ -3,6 +3,8 @@ import "../App/reset.css";
 import "./Projects.scss";
 //* Hooks
 import { Link, useLocation } from "react-router-dom";
+import AECOverview from "./../../assets/projects/AEC_mockup.png";
+import PortfolioOverview from "./../../assets/projects/portfolio_mockup.png";
 
 const Projects: React.FC = () => {
   const location = useLocation();
@@ -43,28 +45,49 @@ const Projects: React.FC = () => {
       </button>
       <div className="projects-list">
         <div className="project">
-          <h5>Projet</h5>
-          <p>Description du projet</p>
-          <img
-            src="https://images.unsplash.com/photo-1522542550221-31fd19575a2d?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="example"
-          />
+          <h5>Mon Portfolio</h5>
+          <p>
+            Réalisé en février 2024, mon portfolio me permet d'expérimenter sur
+            le plan créatif et technique. C'est à la fois ma carte de visite et
+            mon laboratoire d'essai.
+          </p>
+          <div className="img-container">
+            <img src={PortfolioOverview} alt="Aperçu du projet Portfolio" />
+          </div>
+          <p className="project-tools">
+            <h5>Outils utilisés :</h5>
+            <ul>
+              <li>React</li>
+              <li>TypeScript</li>
+              <li>SCSS</li>
+            </ul>
+          </p>
         </div>
         <div className="project">
-          <h5>Projet</h5>
-          <p>Description du projet</p>
-          <img
-            src="https://images.unsplash.com/photo-1522542550221-31fd19575a2d?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="example"
-          />
-        </div>
-        <div className="project">
-          <h5>Projet</h5>
-          <p>Description du projet</p>
-          <img
-            src="https://images.unsplash.com/photo-1522542550221-31fd19575a2d?q=80&w=3270&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt="example"
-          />
+          <h5>Association Entraide Chômeurs</h5>
+          <p>
+            Projet de site vitrine et de conception d'un back-office, pour une
+            association d'aide au retour à l'emploi. Réalisé en 3 semaines en
+            fin de formation par une équipe de 5 développeurs, ce projet est en
+            cours et est voué à être amélioré progressivement. En voici{" "}
+            <a href="https://www.canva.com/design/DAF8lNloy3A/R0XIXYAibrBwTIBBJ_cmUg/view?utm_content=DAF8lNloy3A&utm_campaign=designshare&utm_medium=link&utm_source=editor">
+              un aperçu
+            </a>
+            .
+          </p>
+          <div className="img-container">
+            <img src={AECOverview} alt="Aperçu du projet AEC" />
+          </div>
+          <p className="project-tools">
+            <h5>Outils utilisés :</h5>
+            <ul>
+              <li>React + Redux</li>
+              <li>Symfony</li>
+              <li>TypeScript</li>
+              <li>PHP</li>
+              <li>Tailwind CSS</li>
+            </ul>
+          </p>
         </div>
       </div>
     </section>
