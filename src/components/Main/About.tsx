fdb822fb8ce38d9fc8ee8e8df3e1dfb178064582
MarineLeBec <marinelebec@gmail.com>
2024-02-19 12:37:42 +0100

    feat: adding my portrait to about section

--- src/components/Main/About.tsx
+++ src/components/Main/About.tsx
@@ -3,7 +3,7 @@ import "../App/reset.css";
 import "./About.scss";
 //* Hooks, Components
 import { Link, useLocation } from "react-router-dom";
-import portrait from "../../assets/portrait.png"
+import portrait from "../../assets/portrait.png";
 
 const About: React.FC = () => {
   const location = useLocation();
@@ -42,34 +42,51 @@ const About: React.FC = () => {
           />
         </Link>
       </button>
+
       <div className="portrait">
         <img src={portrait} alt="Mon portrait" />
         <h5>Qui suis-je ?</h5>
+        <img
+          className="icon"
+          width="35"
+          height="35"
+          src="https://img.icons8.com/pastel-glyph/64/884ab2/laptop-coding--v2.png"
+          alt="laptop-coding--v2"
+        />
+        <span>Je m'appelle Marine et je suis développeuse full-stack</span>
+        <img
+          className="icon"
+          width="30"
+          height="30"
+          src="https://img.icons8.com/ios/50/884ab2/heart-puzzle.png"
+          alt="heart-puzzle"
+        />
         <p>
-          <span>Je m'appelle Marine et je suis développeuse full-stack </span>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          at mattis dolor. Praesent vitae lectus sapien. Pellentesque
-          scelerisque gravida nunc in tempor. Class aptent taciti sociosqu ad
-          litora torquent per conubia nostra, per inceptos himenaeos. Cras id
-          luctus nisi. Mauris maximus dui purus, in lobortis arcu lacinia in.
-          Quisque sit amet mi vitae erat interdum tempor. Quisque aliquet quis
-          ante lacinia accumsan. Mauris et dolor vulputate, fermentum ex vitae,
-          vestibulum arcu. Ut maximus mollis metus a ornare. In non diam
-          finibus, consectetur ante a, efficitur metus. Maecenas sapien turpis,
-          interdum non dui id, sodales faucibus ipsum. Morbi in dui vitae dui
-          cursus sollicitudin vitae nec sem. Praesent vitae lectus sapien.
-          Pellentesque scelerisque gravida nunc in tempor. Class aptent taciti
-          sociosqu ad litora torquent per conubia nostra, per inceptos
-          himenaeos. Cras id luctus nisi. Mauris maximus dui purus, in lobortis
-          arcu lacinia in. Quisque sit amet mi vitae erat interdum tempor.
-          Quisque aliquet quis ante lacinia accumsan. Mauris et dolor vulputate,
-          fermentum ex vitae, vestibulum arcu. Ut maximus mollis metus a ornare.
-          In non diam finibus, consectetur ante a, efficitur metus. Maecenas
-          sapien turpis, interdum non dui id, sodales faucibus ipsum. Morbi in
-          dui vitae dui cursus sollicitudin vitae nec sem. Pellentesque non
-          ipsum ipsum. Donec lobortis egestas tellus sed tempor. Fusce eu odio
-          non neque cursus porttitor in sit amet ante. Phasellus laoreet turpis
-          vitae dolor rutrum interdum.
+          Je suis passionnée de musique, de cinéma et de séries. J'aime
+          assembler des grands puzzles, jouer à des jeux vidéos cosy et marcher
+          en forêt;
+        </p>
+        <img
+          className="icon"
+          width="30"
+          height="30"
+          src="https://img.icons8.com/pastel-glyph/64/884ab2/brain--v2.png"
+          alt="brain--v2"
+        />
+        <p>
+          J'ai une personnalité plutôt terre-à-terre, curieuse et empathique;
+        </p>
+        <img
+          className="icon"
+          width="30"
+          height="30"
+          src="https://img.icons8.com/ios/50/884ab2/development-skill.png"
+          alt="development-skill"
+        />
+        <p>
+          Je travaille chaque jour à améliorer ma pratique de code. Je suis
+          passionnée de CSS, d'UX et de TypeScript, et je m'intéresse aux
+          langages côté serveur pour optimiser mes projets.
         </p>
       </div>
     </section>
